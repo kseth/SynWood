5e44669641a8c5a8cdc98b2e8c2cfcee69774701
kseth <k.seth1993@gmail.com>
2012-10-15 15:32:40 -0400

    merge trial2

--- LDsynLikSpat.R
+++ LDsynLikSpat.R
@@ -10,15 +10,15 @@ source("param.r")
 ## load data 
 ## use simulated data as in base_regression.R
 source("maps_basic_regression.R") # gives maps
-source("functions_migration_KS.R")
+source("functions_migration.R")
 
 #==================
 #  Params, if modifying stats: look for multiGilStat
 #==================
-Nrep=300;
+Nrep=500;
 n.mc=30000
 set.seed(1)
-genIntervals <- c(seq(10, 100, 15), seq(130, 250, 30)) # distance classes for the general variogram
+genIntervals <- seq(0, 250, 30) # distance classes for the general variogram
 
 #===================
 # Prep for simulations
@@ -28,45 +28,47 @@ threshold <- 2000
 dist_mat <- nearest.dist(x=sp, y=NULL, method="euclidian", delta=threshold, upper=NULL);          
 dist_mat <- as.matrix(dist_mat)
 
-# Rprof()
 # cumulProbMat <- fast_prob_mat(halfDistJ, halfDistH, useDelta, delta, rateHopInMove, rateSkipInMove, rateJumpInMove, threshold, sp, dist_mat,SB,cumul=TRUE)
-  cumulProbMat <- generate_prob_mat(halfDistJ, halfDistH, useDelta, delta, rateHopInMove, rateSkipInMove, rateJumpInMove, threshold, sp, dist_mat,SB,cumul=TRUE)
-# Rprof(NULL)
+cumulProbMat <- generate_prob_mat(halfDistJ, halfDistH, useDelta, delta, rateHopInMove, rateSkipInMove, rateJumpInMove, threshold, sp, dist_mat,SB,cumul=TRUE)
 
 # test cumulProbMat
 test_that("cumulProbMat is cumulative, ending in ones",{
 expect_equal(sum(cumulProbMat[dim(cumulProbMat)[1],]),dim(cumulProbMat)[1])
 expect_true(all.equal(cumulProbMat[dim(cumulProbMat)[1],],rep(1,dim(cumulProbMat)[1])))
 })
+# all.equal(cumulProbMatRef,cumulProbMat)
 
 ### the vector of stats for the data
 infestH<-which(maps$infest3==1)
-stats <- multiGilStat(cumulProbMat=cumulProbMat, blockIndex = blockIndex, infestH = infestH, timeH=maps$ages[infestH], endTime = 1, rateMove = rateMove, Nrep = Nrep, coords = maps[, c("X", "Y")], breaksGenVar = genIntervals, simul=FALSE)
+stats <- multiGilStat(cumulProbMat=cumulProbMat, blockIndex, infestH, timeH=maps$ages[infestH], endTime = 1, rateMove, Nrep, coords = maps[, c("X", "Y")], breaks = genIntervals, simul=FALSE)
 statsData <- stats$statsTable[, 1]
 
 ### the standard call to multiGilStat
 snapTimes<-attributes(maps)$snapTimes
 nbit<-snapTimes[3]-snapTimes[2]
 infestH<-which(maps$infest2==1)
-outBase <- multiGilStat(cumulProbMat=cumulProbMat, blockIndex = blockIndex, infestH = infestH, timeH=rep(-1,length(infestH)), endTime = nbit, rateMove = rateMove, Nrep = Nrep, coords = maps[, c("X", "Y")], breaksGenVar = genIntervals, simul=TRUE)
-
+outBase <- multiGilStat(cumulProbMat=cumulProbMat, blockIndex, infestH, timeH=rep(-1,length(infestH)), endTime = nbit, rateMove, Nrep, coords = maps[, c("X", "Y")], breaks = genIntervals, simul=TRUE)
 # test
 test_that("multiGilStat with true params -> stats ~= statsData",{
 expect_true(count(apply(outBase$statsTable,1,max)<statsData)==0)
 expect_true(count(apply(outBase$statsTable,1,min)>statsData)==0)
 })
 
-stop()
-
 ### starting point for simulations
 infestH <- which(maps$infest2 > 0)
 timeH <- maps$ages[infestH]
 
 ### Priors (also the place to change the parameters)
-priorMeans<-log(c(0.03, 0.04, 0.35))
-realMeans<-log(c(rateMove, rateJumpInMove, delta))
-names(priorMeans)<-c("rateMove", "rateJumpInMove", "delta")
+priorMeans<-log(c(rateMove))
+realMeans<-log(c(rateMove))
+names(priorMeans)<-c("rateMove")
 names(realMeans)<-names(priorMeans)
+
+### Intervals of definition for the parameters
+paramInf<-c(0.002)
+paramSup<-c(0.24)
+names(paramInf)<-names(priorMeans)
+names(paramSup)<-names(priorMeans)
 
 ### LD formalism for Data (no setting should be made past this declaration)
 PGF<-function(Data){ # parameters generating functions (for init etc...)
@@ -75,13 +77,10 @@ PGF<-function(Data){ # parameters generating functions (for init etc...)
   return(values)
 }
 
-### Set cumulProbMat to NULL <- we're now using priorMeans from 3 valuables
-### Must calculate probMat each time
 MyData <- list(y=statsData,
 	     trans=NULL,
-	     cumulProbMat=NULL,
+	     cumulProbMat=cumulProbMat,
 	     blockIndex=blockIndex,
-	     dist_out = makeDistClassesWithStreets(X = as.vector(maps[, "X"]), Y = as.vector(maps[, "Y"]), genIntervals, blockIndex), 
 	     infestH=infestH,
 	     timeH=timeH,
 	     endTime=nbit,
@@ -90,6 +89,8 @@ MyData <- list(y=statsData,
 	     Nrep=Nrep,
 	     priorMeans=priorMeans,
 	     genIntervals=genIntervals,
+	     paramInf=paramInf,
+	     paramSup=paramSup,
 	     PGF=PGF,
 	     mon.names=c("ll","lp"), # monitored variables (like in Model)
 	     parm.names=names(priorMeans)# parameters names (like in Model and Initial.Values)
@@ -97,69 +98,42 @@ MyData <- list(y=statsData,
 
 rm(cumulProbMat,dist_mat)
 
+### "Pirat" data (global because need to be updated)
+minLLever=-10e6
+
 #==================================
 ## declare Model for LD
 #==================================
-Model <- function(theta,Data,postDraw=FALSE){
+Model<-function(theta,Data,postDraw=FALSE,infestHints=NULL){
 	# coerce theta, a priori all positive
 	theta<-exp(theta)
 	names(theta)<-Data$parm.names
-	
-	# set intervals for the variables
-	# if value above the interval, move to closest value
-	theta["rateMove"]<-interval(theta["rateMove"],a=0.001, b=0.22)
-	theta["rateJumpInMove"]<-interval(theta["rateJumpInMove"],a=0.0001, b = 0.25)
-	theta["delta"]<-interval(theta["delta"], a=0.0001, b = 0.5)	
-
-	cat("theta:",theta, "\n")
-
-	seed<-runif(1,min=0,(2^16-1)) # NB: for repeatability, R seed 
-					#     Should be set
-		
+	# theta["rateMove"]<-interval(theta["rateMove"],a=0.002,b=0.24)
+	theta["rateMove"]<-interval(theta["rateMove"],a=Data$paramInf["rateMove"],b=Data$paramSup["rateMove"])
+	names(theta)<-Data$parm.names
+	cat("theta:",theta)
 
 	# simulations
 	start <- Sys.time()
+	seed<-runif(1,min=0,(2^16-1)) # NB: for repeatability, R seed 
 	if(postDraw){
 		getStats<-FALSE
-			# then in wrapper use infested if Nrep =1 and infested$Dens if Nrep=a few
+					#     Should be set
+		
+		# then in wrapper use infested if Nrep =1 and infested$Dens if Nrep=a few
 		# to show a posterior predictive map
 	}else{
 		getStats<-TRUE
 	}
-	
-	# if only optimization is rateMove
-	if(length(theta) == 1 & all.equal(Data$parm.names[1], "rateMove")){
-		out <- multiGilStat(cumulProbMat = Data$cumulProbMat, blockIndex = Data$blockIndex, 
-			    infestH = Data$infestH, timeH = Data$timeH, endTime = Data$nbit, 
-			    rateMove = theta["rateMove"], 
-			    Nrep = Data$Nrep, 
+	out <- multiGilStat(Data$cumulProbMat, Data$blockIndex, 
+			    Data$infestH, Data$timeH, endTime = Data$nbit, 
+			    theta["rateMove"], 
+			    Data$Nrep, 
 			    coords = Data$maps[, c("X", "Y")], 
-			    breaksGenVar = Data$genIntervals,
+			    breaks = Data$genIntervals,
 			    getStats=getStats,
-			    seed=seed,
-			    dist_out = Data$dist_out)
-	}else{
-		#pass all variables correctly!
-		#need to make probMat everytime, this is more time consuming.
-		#note rateHopInMove = 1-rateJumpInMove
-		out <- multiGilStat(cumulProbMat = Data$cumulProbMat, blockIndex = Data$blockIndex, 
-			    infestH = Data$infestH, timeH = Data$timeH, endTime = Data$nbit, 
-			    rateMove = theta["rateMove"], 
-			    Nrep = Data$Nrep, 
-			    coords = Data$maps[, c("X", "Y")], 
-			    breaksGenVar = Data$genIntervals,
-			    getStats=getStats,
-			    seed=seed,
-			    halfDistJ = ifelse(is.na(theta["halfDistJ"]), halfDistJ, theta["halfDistJ"]), 
-			    halfDistH = ifelse(is.na(theta["halfDistH"]), halfDistH, theta["halfDistH"]), 
-			    useDelta = ifelse(is.na(theta["useDelta"]), useDelta, theta["useDelta"]), 
-			    delta = ifelse(is.na(theta["delta"]), delta, theta["delta"]), 
-			    rateHopInMove = ifelse(is.na(theta["rateJumpInMove"]), 1-rateJumpInMove, 1-theta["rateJumpInMove"]), 
-			    rateSkipInMove = ifelse(is.na(theta["rateSkipInMove"]), rateSkipInMove, theta["rateSkipInMove"]), 
-			    rateJumpInMove = ifelse(is.na(theta["rateJumpInMove"]), rateJumpInMove, theta["rateJumpInMove"]),
-			    dist_out = Data$dist_out)
-	}
-
+			    seed=seed
+			    )
 	end <- Sys.time()
 	cat("t multiGil:",end-start,"\n")
 
@@ -169,8 +143,20 @@ Model <- function(theta,Data,postDraw=FALSE){
 		LP<-NA
 	}else{
 		yhat<-out$statsTable[,1]
+
+		statsToKeep<-c(1:16,39:42)
+		statsTable<-out$statsTable[statsToKeep,]
+		y<-Data$y[statsToKeep]
+
+		# statsTable<-out$statsTable[!duplicated(yhat),]
+	
 		# synthetic likelihood
-		ll<-synLik(out$statsTable,Data$y,Data$trans)
+		success<-try(ll<-synLik(statsTable,y,Data$trans))
+		if(class(success)=="try-error"){
+		  ll<-minLLever
+		}else{
+		  minLLever<<-min(ll,minLLever)
+		}
 
 		# get likelihood with priors
 		LL<-ll
@@ -188,62 +174,57 @@ Model <- function(theta,Data,postDraw=FALSE){
 			 # will be used for posterior check
 			 parm=log(theta) # the parameters, possibly constrained by the model
 			 )
-}
-
- 
+} 
 #===========================
 # Testing Model/Data
 #===========================
 ModelOutGood<-Model(priorMeans,MyData)
 ModelOutBest<-Model(realMeans,MyData)
-expect_true(ModelOutGood$Dev>ModelOutBest$Dev)
+expect_true(ModelOutGood$Dev>ModelOutBest$Dev-4)
 
+theta<-realMeans
+theta["rateMove"]<-log(0.5)
+ModelOutBest<-Model(theta,MyData)
+
+testRateMove<-seq(0.025,0.05,0.0005)
+testLL<-0*testRateMove
+for(i in 1:length(testRateMove)){
+	rM<-testRateMove[i]
+	cat("RateMove:",rM," ")
+	theta["rateMove"]<-log(rM)
+	ModelOutGood<-Model(theta,MyData)
+	testLL[i]<-ModelOutGood$LP
+}
 signedLog<-function(signedBigNums){
 	signNum<-sign(signedBigNums)
 	signedLog<-log(abs(signedBigNums))*signNum
 	return(signedLog)
 }
 
-# theta<-realMeans
-# theta["rateMove"]<-log(0.5)
-# ModelOutBest<-Model(theta,MyData)
-
-# testlogRateMove<-seq(-7,-1.5,0.1)
-# testLL<-0*testlogRateMove
-# for(i in 1:length(testlogRateMove)){
-#	rM<-exp(testlogRateMove[i])
-#	cat("RateMove:",rM," ")
-#	theta["rateMove"]<-log(rM)
-#	ModelOutGood<-Model(theta,MyData)
-# 	testLL[i]<-ModelOutGood$LP
-# }
-# plot(testlogRateMove,signedLog(testLL),type="l",ylab="Log(Log(likelihood)",xlab="Log(rateMove)",main="Synthetic likelihood profile for rateMove \n blue is true");
-# abline(v=log(rateMove),col="blue")
+par(mfrow=c(1,2))
+plot(log(testRateMove),signedLog(testLL),type="l",ylab="Log(Log(likelihood))",xlab="Log(rateMove)",main="Synthetic likelihood profile for rateMove \n blue is true");
+abline(v=log(rateMove),col="blue")
+int<-0.5
+# sel<-which(log(testRateMove) > log(rateMove)-int & log(testRateMove) < log(rateMove)+int)
+sel<-which(testLL > -0)
+plot(testRateMove[sel],exp(testLL[sel]),type="l",ylab="Log(likelihood)",xlab="Log(rateMove)",main="Synthetic likelihood profile for rateMove \n blue is true (Zoom)");
+abline(v=rateMove,col="blue")
 # dev.print(device=pdf,"SynLikProfileRateMove.pdf")
-
-
-# weibull order plotting to check if stats are normal
-for(i in 1:dim(outBase$statsTable)[1])
-{
- 
- order <- order(outBase$statsTable[i, ])
- plot((1:dim(outBase$statsTable)[2])/(dim(outBase$statsTable)[2]+1), outBase$statsTable[i, order], main = i)
- Sys.sleep(0.5)
- 
-}
+# save.image("profilSynLLH_rateMove.img")
 stop()
+
 #===========================
 # Init values 
 #===========================
 
-Initial.Values <- GIV(Model, MyData, PGF=TRUE,n=100) #GIV: generate initial values
+# Initial.Values <- GIV(Model, MyData, PGF=TRUE,n=100) #GIV: generate initial values
+Initial.Values <- priorMeans
 
 #===========================
 ## launch LD
 #===========================
 
 start1<-Sys.time()
-n.mc <- 200
 Fit1 <- LaplacesDemon(Model, 
 		     Data=MyData, 
 		     Initial.Values, 
@@ -265,7 +246,7 @@ Consort(Fit1)
 
 # PosteriorChecks(Fit1)
 MyData$infestH<-which(maps$infest3==1)
-postMap<-getPosteriorMaps(Fit1,MyData,repByTheta=10)
+postMap<-getPosteriorMapsLD(Fit1,MyData,repByTheta=10)
 
 # Brier scores
 predReg<-maps$predict4
