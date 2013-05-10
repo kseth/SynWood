4940fcd36de0944f56271f8242a46d72b7e0cac6
Corentin Mario Barbu <corentin.barbu@gmail.com>
2013-05-10 13:35:05 -0400

    Bash "make" of the c

---
+++ SynWood_compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: SynWood_compile.sh
+# 
+#         USAGE: ./SynWood_compile.sh 
+# 
+#   DESCRIPTION: Allow to compile in one line everything needed for SynWood
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: gsl (on ubuntu libgsl0-dev)
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHORS: KS, CB
+#  ORGANIZATION: 
+#       CREATED: 05/09/2013 03:31:25 PM EDT
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+R CMD SHLIB -lgsl -lgslcblas -lm functions_migration.c 
+
+
+
+
